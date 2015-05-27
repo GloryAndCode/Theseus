be7e6021a5651a72c6379ecec462287906f102a7
Cassandra Cruz <celkamada@gmail.com>
2015-05-27 02:16:09 -0700

    Added constructor and method signatures for Dispatcher class Wrote JSDoc-formatted comments for all functions

---
+++ client/js/dispatcher.js
@@ -0,0 +1,59 @@
+// Require statements for view and fileSystem goes here
+//var fs = require('./fileSystem.js');
+//var view = require('./view.js');
+
+/**
+ * Creates Dispatcher instance
+ * @constructor
+ * @param {FileSystem} fileSystem - instance of FileSystem for dispatcher to use
+ * @param {View} view - instance of View for dispatcher to use
+ * @return {Dispatcher} returns initialized Dispatcher instance
+ */
+var Dispatcher = function() {
+  this.runningApps = {};
+};
+
+/**
+ * Initializes application
+ * @param {String} script - uri for application script
+ */
+Dispatcher.prototype.initApp = function(script) {
+  
+};
+
+/**
+ * Handles fileSystem reads for apps
+ * @param {String} appID - internal reference to an app
+ * @param {String} fileName - full path to the file
+ */
+Dispatcher.prototype.fileRequest = function(appID, fileName) {
+  
+};
+
+/**
+ * Handles fileSystem writes for apps
+ * @param {String} appID - internal reference to an app
+ * @param {String} fileName - full path to the file
+ * @param {Object} data - data to write to file
+ */
+Dispatcher.prototype.fileWrite = function(appID, fileName, data) {
+  
+};
+
+/**
+ * Updates Canvas element associated with an app
+ * @param {String} appID - internal reference to an app
+ * @param {String} method - method or property on canvas to run / set
+ * @param {Array}  data - Arguments for function or data for property
+ */
+Dispatcher.prototype.canvasUpdate = function(appID, method, data) {
+  
+};
+
+/**
+ * Closes application
+ * @param {String} appID - internal reference to an app
+ */
+Dispatcher.prototype.closeApp = function(appID) {
+  
+};
