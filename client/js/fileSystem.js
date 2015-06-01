fb74a8273653be3860d250d9193b767ebbcb2409
Cassandra Cruz <celkamada@gmail.com>
2015-06-01 15:02:22 -0700

    Created fileSystem module that interfaces directly with server.

---
+++ client/js/fileSystem.js
@@ -0,0 +1,44 @@
+/**
+ * File system module for Theseus
+ * Backed by server itself
+ */
+
+/**
+ * Initializer for Theseus
+ * @constructor
+ */
+var FileSystem = function() {
+  
+};
+
+/**
+ * Reads file from server
+ * @param {String} appID - internal ID of app
+ * @param {String} fileName - name of file to grab
+ */
+FileSystem.prototype.readFile = function (appID, fileName) {
+  return m.request({
+    method: "POST",
+    url: "/getFile",
+    background: true,
+    data: {fileName: fileName}
+  });
+};
+
+/**
+ * Writes file to server
+ * @param {String} appID - internal ID of app
+ * @param {String} fileName - name of file to write
+ * @param {ANY} data - any data that can be serialized to JSON
+ */
+FileSystem.prototype.writeFile = function (appID, fileName, data) {
+  return m.request({
+    method: "POST",
+    url: "/storeFile",
+    background: true,
+    data: {
+      filename: fileName,
+      fileContents: data
+    }
+  });
+};
