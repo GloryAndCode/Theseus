66a2e3791c6a49d2576f4e5919c0e1314b6d807c
kf6nux <kf6nux@users.noreply.github.com>
2015-06-03 16:20:03 -0700

    demo app now clears canvas before every draw

--- client/js/demoApps/helloWorld.js
+++ client/js/demoApps/helloWorld.js
@@ -1,16 +1,15 @@
-
 var counter = 0;
 
 var updateCounter = function() {
   counter++;
-  postMessage({command: "canvasUpdate", args: ['2d', [['fillText', [counter, 10, 10]]]]});
+  postMessage({command: 'canvasUpdate', args: ['2d', [['clearRect', [0,0,9999,9999]], ['fillText', [counter + ' Hello World!', 5, 25]]]]});
   setTimeout(updateCounter, 1000);
 };
 
-postMessage({command: "canvasUpdate", args: ['2d', [
+postMessage({command: 'canvasUpdate', args: ['2d', [
   ['fillStyle', 'white'],
-  ['fill', []],
   ['font', '30px Arial'],
-  ['fillText', ['Hello World!', 10, 50]]
 ]]});
+
 updateCounter();
+
