38bb5cb74e1617c8709ff50465a5594642012052
Cassandra Cruz <celkamada@gmail.com>
2015-06-11 03:09:40 -0700

    Fixed device orientation controls, set up stereo effect to only activate if device orientation is available.

--- client/js/view.js
+++ client/js/view.js
@@ -48,11 +48,20 @@ View.prototype.init = function() {
   this.container = document.getElementById('example');
   this.container.appendChild(this.element);
 
+  this.orientationBinding = this.setOrientationControls.bind(this);
+
   // Make the 3d work
-  this.effect = new THREE.StereoEffect(this.renderer);
+  var isMobile = /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(navigator.userAgent)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent);
+  if (window.DeviceOrientationEvent !== undefined && isMobile) {
+    this.effect = new THREE.StereoEffect(this.renderer);
+    window.addEventListener('deviceorientation',
+                          this.orientationBinding,
+                          true);
+  };
 
   // Add a scene and a camera
   this.scene = new THREE.Scene();
+
   this.camera = new THREE.PerspectiveCamera(90,1,0.001,700);
   this.camera.position.set(0,10,0);
   this.scene.add(this.camera);
@@ -67,11 +76,6 @@ View.prototype.init = function() {
   );
   this.controls.noZoom = true;
   this.controls.noPan = true;
-
-  // Manage device orientation
-  window.addEventListener('deviceorientation',
-                          this.setOrientationControls,
-                          true);
 
   // Add some light to the scene
   this.light = new THREE.HemisphereLight(0x777777, 0x000000, 0.6);
@@ -155,13 +159,16 @@ View.prototype.setOrientationControls = function(e) {
     return;
   }
 
+  console.log('' + e.alpha + ' ' + e.beta + ' ' + e.gamma);
+
   this.controls = new THREE.DeviceOrientationControls(this.camera, true);
   this.controls.connect();
   this.controls.update();
 
-  this.element.addEventListener('click', this.fullscreen, false);
+  debugger;
+  this.element.addEventListener('click', this.fullscreen.bind(this), false);
   window.removeEventListener('deviceorientation',
-                             this.setOrientationControls,
+                             this.orientationBinding,
                              true);
 };
 
@@ -175,7 +182,7 @@ View.prototype.resize = function() {
     this.camera.updateProjectionMatrix();
     // Update the renderer
     this.renderer.setSize(width, height);
-    this.effect.setSize(width, height);
+    this.effect ? this.effect.setSize(width, height) : null;
   }
 };
 
@@ -188,7 +195,9 @@ View.prototype.update = function(dt) {
 
 // Rendering method
 View.prototype.render = function(dt) {
-  this.effect.render(this.scene, this.camera);
+  this.effect ?
+  this.effect.render(this.scene, this.camera) :
+  this.renderer.render(this.scene, this.camera);
 };
 
 // Animate everything and run the render loop
@@ -209,13 +218,13 @@ View.prototype.animate = function(t) {
 // Method for requesting fullscreen depending on the browser
 View.prototype.fullscreen = function() {
   if (this.container.requestFullscreen) {
-  	this.container.requestFullscreen();
+    this.container.requestFullscreen();
   } else if (this.container.msRequestFullscreen) {
-  	this.container.msRequestFullscreen();
+    this.container.msRequestFullscreen();
   } else if (this.container.mozRequestFullScreen) {
-  	this.container.mozRequestFullScreen();
+    this.container.mozRequestFullScreen();
   } else if (this.container.webkitRequestFullscreen) {
-  	this.container.webkitRequestFullscreen();
+    this.container.webkitRequestFullscreen();
   }
 };
 
@@ -282,8 +291,8 @@ View.prototype.addScreen = function(position, size, screenNumber) {
     side: THREE.DoubleSide
   });
   var newScreen = new THREE.Mesh(
-  	screenGeo,
-  	screenMat
+    screenGeo,
+    screenMat
   );
   newScreen.screenNumber = screenNumber;
 
@@ -439,3 +448,4 @@ View.prototype.addHighlight = function(position, size) {
 };
 
 module.exports = View;
+
